28209eff3910883e7b920bb8e4684ce1be0d282f
Maxim Brizhatyy <max.brijatyy@yandex.ru>
2024-11-03 00:34:23 +0700

    Начальный коммит: 1. внедрены зависимости 2. описаны настройки в application.properties 3. созданы *.sql файлы для автогенерации таблицы с книгами.

---
+++ src/main/java/com/smsfinance/smsfinance/SmsfinanceApplication.java
@@ -0,0 +1,13 @@
+package com.smsfinance.smsfinance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmsfinanceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmsfinanceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/smsfinance/smsfinance/SmsfinanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smsfinance.smsfinance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmsfinanceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
